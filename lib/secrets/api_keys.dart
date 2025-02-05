f850e627bee70778d63829e1573259191b192579
vaishnavi <nayakvaishnavi88@gmail.com>
2025-02-05 06:13:21 +0530

    updated

---
+++ lib/secrets/api_keys.dart
@@ -0,0 +1,8 @@
+// secrets/api_keys.dart
+//const String projectURL = 'https://fohcmrducrdylsetimwa.supabase.co';
+
+//const String apiKey =
+ //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvaGNtcmR1Y3JkeWxzZXRpbXdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExNDg2NjUsImV4cCI6MjA0NjcyNDY2NX0.q1ifCc2Tz8NN09ahw8kIyjU0jXG8Tgp2vEWS-iKGXVc';
+const String projectURL = String.fromEnvironment('PROJECT_URL');
+const String apiKey = String.fromEnvironment('API_KEY');
+
